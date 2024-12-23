b975423f1077932b3fcd51fdd1bea4c7cb9c6b20
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-12-23 13:33:31 +0000

    firstcommit on the counter

--- app/components/counter.tsx
+++ app/components/counter.tsx
@@ -1,10 +1,22 @@
+import {Minus, Plus} from "lucide-react"
+
 export default function Counter() {
     return (
-        <div className="max-w-3xl mx-auto">
+        <div className="max-w-3xl mx-auto ">
             <h2 className="scroll-m-20 text-4xl font-semibold tracking-tight first:mt-0">
-                The People of the Kingdom
+                Redux Counter
             </h2>
-            <div className="py-4"></div>
+            <div className="py-4 ">
+                <div className="flex flex-row items-center justify-center space-x-4">
+                  <button>
+                    <Minus/>
+                  </button>
+                  <p>0</p>
+                  <button>
+                    <Plus/>
+                  </button>
+                </div>
+            </div>
         </div>
     )
 }
