3938c61a5943c4f6628134f322afdf82cba08c9e
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-12-23 13:21:35 +0000

    This is our begining

---
+++ app/components/counter.tsx
@@ -0,0 +1,10 @@
+export default function Counter() {
+    return (
+        <div className="max-w-3xl mx-auto">
+            <h2 className="scroll-m-20 text-4xl font-semibold tracking-tight first:mt-0">
+                The People of the Kingdom
+            </h2>
+            <div className="py-4"></div>
+        </div>
+    )
+}
