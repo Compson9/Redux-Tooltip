b83a11ac640557e39ccc9328f2d21f8efe6370c8
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-12-23 13:45:52 +0000

    This is our second way to the file in redux

--- app/components/counter.tsx
+++ app/components/counter.tsx
@@ -1,20 +1,20 @@
-import {Minus, Plus} from "lucide-react"
+import { Minus, Plus } from "lucide-react"
 
 export default function Counter() {
     return (
-        <div className="max-w-3xl mx-auto ">
-            <h2 className="scroll-m-20 text-4xl font-semibold tracking-tight first:mt-0">
+        <div className="max-w-3xl flex flex-col justify-center items-center mx-auto ">
+            <h2 className="scroll-m-20 text-4xl text-white font-semibold tracking-tight first:mt-0">
                 Redux Counter
             </h2>
-            <div className="py-4 ">
-                <div className="flex flex-row items-center justify-center space-x-4">
-                  <button>
-                    <Minus/>
-                  </button>
-                  <p>0</p>
-                  <button>
-                    <Plus/>
-                  </button>
+            <div className="py-10 ">
+                <div className="flex items-center justify-center space-x-4">
+                    <button >
+                        <Minus color="white" size={40} />
+                    </button>
+                    <p className="text-[30px] text-white">0</p>
+                    <button>
+                        <Plus color="white" size={40} />
+                    </button>
                 </div>
             </div>
         </div>
--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -2,7 +2,7 @@ import Counter from "../components/counter";
 
 export default function Page(){
     return (
-        <div>
+        <div className="py-8 px-8 h-screen bg-blue-600">
            <Counter/>
         </div>
     )
