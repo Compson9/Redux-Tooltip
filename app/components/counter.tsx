b46aa21a1780aa2f95e1f21f14424012de54e5df
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-12-24 22:27:20 +0000

    The christmas evening

--- app/components/counter.tsx
+++ app/components/counter.tsx
@@ -16,6 +16,7 @@ export default function Counter() {
         dispatch(decrement())
     }
 
+    
     return (
         <div className="max-w-3xl flex flex-col justify-center items-center mx-auto ">
             <h2 className="scroll-m-20 text-4xl text-white font-semibold tracking-tight first:mt-0">
